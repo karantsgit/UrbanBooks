4843e81369b7efb0d062e78972dddaf0a6575056
Karan Trivedi <karan.t@sgit.in>
2017-11-29 10:28:29 +0530

    Added Files..

---
+++ UrbanBooks.Entity/PurchaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanBooks.Entity
+{
+    public class PurchaseModel
+    {
+        [Required(ErrorMessage="Name is Required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage="Address is Required.")]
+        public string Address { get; set; }
+        [Required(ErrorMessage="Delivery Address is Required.")]
+        public string DeliveryAddress { get; set; }
+    }
+}
